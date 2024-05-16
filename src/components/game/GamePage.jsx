11857780a57cd878e06ab466d569d2e672e03ca4
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-05-15 22:59:15 -0300

    creating basic front and going to the back end

--- src/components/game/GamePage.jsx
+++ src/components/game/GamePage.jsx
@@ -1,23 +1,46 @@
 import React, { useState } from 'react';
 import Coin from '../../assets/imgs/icons/coinicon.png'
+import CharMonster from '../../assets/imgs/npcskins/monster.png';
+import MyChar from '../../assets/imgs/npcskins/char1.png'
 import './game.css'
 
 const Game = () => {
   const [coinCliked, setCoinCliked] = useState(false)
-  const [coins,setCoins] = useState(0)
+  const [coinClikedScale, setCoinClikedScale] = useState(false)
+  const [coins, setCoins] = useState(0)
 
-  const HandleCoinClicked =()=>{
-
+  const HandleCoinClicked = () => {
+    setCoinCliked(true)
+    if (coinCliked === true) {
+      setCoinClikedScale(true)
+      setTimeout(() => {
+        setCoinClikedScale(false)
+      }, 100);
+    }
+    setCoins(coins + 1)
   }
   return (
     <div>
-      <header>
-        {coinCliked}
+      <header className='header_game_coin'>
+        <div className='header_game_coin_container'>
+          <div className='header_game_coin_point'>
+            <span className='coins_point'>{coins}</span>
+          </div>
+        </div>
       </header>
+      <main className='main_game_window_container'>
+        <div className='game_window'>
+          <div className='game_window_bottom'>
+          <img className='game_char game_char_mychar' src={MyChar} alt="" />
+          <img className='game_char game_char_monster' src={CharMonster} alt="" />
+          </div>
+         
+        </div>
+      </main>
       <footer className='footer_game_coin'>
         <div className='footer_game_coin_container'>
           <div>
-            <img onClick={HandleCoinClicked} className='coin' src={Coin} alt="" />
+            <img onClick={HandleCoinClicked} className={coinClikedScale ? 'coinCliked' : 'coin'} src={Coin} alt="" />
           </div>
         </div>
       </footer>
