a910f78bb01e88011fc142f04d5eb8633af060d7
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-05-22 09:59:41 -0300

    fix logic enemy and add status my char

--- src/components/game/GamePage.jsx
+++ src/components/game/GamePage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Coin from '../../assets/imgs/icons/coinicon.png';
 import StartButton from '../../assets/imgs/icons/startButton.png'
+import GoButton from '../../assets/imgs/icons/goButton.png'
 import CharMonster from '../../assets/imgs/npcskins/monster.png';
 import MyCharImg from '../../assets/imgs/npcskins/char1.png';
 
 
 import EnemyDefeat from '../../assets/imgs/icons/enemydefeat.png';
-
+import LevelUp from '../../assets/imgs/icons/levelup.png'
 
 
 import Arena from '../../assets/imgs/arena.jpeg';
@@ -33,43 +34,75 @@ const Game = () => {
 
 
   //Characters Current function Show
-  const myCharWizard = myCharCurrent.charWizard
+
   const showMycharWizardStatus = () => {
 
   }
 
   //Witch
-  const witch = enemies.witch
+
   useEffect(() => {
-   
+
 
   }, []);
 
 
   //start battle
+  const witch = enemies.witch
   const [witchHpCurrent, setWitchHpCurrent] = useState(witch.hp);
-  const [updateWitchHpMax,setUpdateWitchHpMax]=useState(witch.hpmax)
+  const [updateWitchHpMax, setUpdateWitchHpMax] = useState(witch.hpmax)
+  const [updateWitchAttack, setUpdateWitchAttack] = useState(witch.atk)
+  const [updateWitchPower, setUpdateWitchPower] = useState(witch.power);
+  const [updateWitchLevel, setUpdateWitchLevel] = useState(witch.level);
 
-  const [witchLevelCurrent, setWitchLevelCurrent] = useState(witch.level);
 
-  const [battleWins,setBattleWins]= useState(false)
-  
+  const myCharWizard = myCharCurrent.charWizard
+  const [myCharHpCurrent, setMyCharHpCurrent] = useState(myCharWizard.hp);
+  const [updateMyCharHpMax, setUpdateMyCharHpMax] = useState(myCharWizard.hpmax);
+  const [updateMyCharAttack, setUpdateMyCharAttack] = useState(myCharWizard.atk);
+  const [updateMyCharPower, setUpdateMyCharPower] = useState(myCharWizard.power);
+  const [updateMyCharLevel, setUpdateMyCharLevel] = useState(myCharWizard.level);
+
+
+  //const [updateMyCharPower, setUpdateMyCharPower] = useState(myCharWizard.power);
+
+
+  const [battleWins, setBattleWins] = useState(false)
+
   const myCharAttack = myCharWizard.atk;
   const enemyWitchHp = updateWitchHpMax;
-  
-  const handleStartBattle = () => {
-    battle(battleWins,myCharAttack, setWitchHpCurrent,setUpdateWitchHpMax,setWitchLevelCurrent);
-    console.log(enemyWitchHp)
-    console.log(witchHpCurrent)
-    setBattleWins(false)
-    console.log(battleWins)
+
+  const handleStartBattle = async () => {
+    try {
+      const results = await battle(
+        battleWins,
+        setWitchHpCurrent,
+        setUpdateWitchHpMax,
+        setUpdateWitchLevel,
+        setUpdateWitchAttack,
+        setUpdateWitchPower,
+        //myChar >>
+
+        setMyCharHpCurrent,
+        setUpdateMyCharHpMax,
+        setUpdateMyCharLevel,
+        setUpdateMyCharAttack,
+        setUpdateMyCharPower,
+
+      );
+      //show level up
+      setTimeout(() => {
+        setBattleWins(false);
+      }, 2000);
+      setBattleWins(results);
+    } catch (error) {
+      console.error('Error during battle:', error);
+    }
+
   };
-
-
-
-
   //click coin
   const HandleCoinClicked = () => {
+    console.log(setUpdateMyCharAttack)
     setCoinCliked(true)
     if (coinCliked === true) {
       setChangeScale(true)
@@ -92,15 +125,16 @@ const Game = () => {
       <main className='main_game_window_container'>
 
         <div className='view_status_container view_status_container_mychar '>
-          <p>Level: {myCharWizard.level}</p>
-          <p>Life: {myCharWizard.hpmax}</p>
-          <p>Attack: {myCharWizard.atk}</p>
-          <p>Power: {myCharWizard.power}</p>
+          <p>Level: {updateMyCharLevel}</p>
+          <p>Life: {myCharHpCurrent}/{updateMyCharHpMax}</p>
+          <p>Attack: {updateMyCharAttack}</p>
+          <p>Power: {updateMyCharPower}</p>
         </div>
 
         <div>
           <div className='game_status_container'>
             {witchHpCurrent <= 0 ? <img className='game_window game_image_enemydefeat' src={EnemyDefeat} /> : ''}
+           {battleWins === true ? <img className='game_window game_image_levelup' src={LevelUp} /> : ''} 
           </div>
           <div className='game_window'>
             <img className='game_window' src={Arena} alt="" />
@@ -113,14 +147,15 @@ const Game = () => {
           <div className='game_functions_container'>
             <div className='game_functions'>
               <img onClick={handleStartBattle} className={changeScaleStartButton ? 'game_functions_button start_button' : 'game_functions_button start_button_clicked'} src={StartButton} alt="" />
+              <img className={changeScaleStartButton ? 'game_functions_button start_button' : 'game_functions_button start_button_clicked'} src={GoButton} alt="" />
             </div>
           </div>
         </div>
         <div className='view_status_container view_status_container-enemy '>
-          <p>Level: {witchLevelCurrent}</p>
+          <p>Level: {updateWitchLevel}</p>
           <p>Life: {witchHpCurrent}/{updateWitchHpMax}</p>
-          <p>Attack: {witch.atk}</p>
-          <p>Power: {witch.power}</p>
+          <p>Attack: {updateWitchAttack}</p>
+          <p>Power: {updateWitchPower}</p>
         </div>
         <div className='battle_status_container'>
 
--- src/models/battle.mjs
+++ src/models/battle.mjs
@@ -2,42 +2,100 @@ import { myCharCurrent, enemies } from './characters.js';
 export let battleWins = (false)
 //console.log(myCharCurrent, enemies)
 // defining const of battle >
-export let myCharAttack = myCharCurrent.charWizard.atk;
+let myCharAttack = myCharCurrent.charWizard.atk;
+let myCharhHpMax = myCharCurrent.charWizard.hpmax
+let myCharHp = myCharhHpMax
+let myCharLevel = myCharCurrent.charWizard.level
 
-export const enemyWitchAttack = enemies.witch.atk;
-export let enemyWitchHpMax = enemies.witch.hpmax
 
-export let enemyWitchHp = enemyWitchHpMax
-export let enemyWitchLevel = enemies.witch.level
+let myCharHpCurrent = myCharCurrent.charWizard.hp;
+let myCharAttackCurrent = myCharCurrent.charWizard.atk;
+let myCharPowerCurrent = myCharCurrent.charWizard.power;
+
+//enemy Status
+
+let enemyWitchAttack = enemies.witch.atk;
+let enemyWitchHpMax = enemies.witch.hpmax
+let enemyWitchHp = enemyWitchHpMax
+let enemyWitchLevel = enemies.witch.level
 
 let witchHpCurrent = enemies.witch.hp;
+let witchAttackCurrent = enemies.witch.atk;
+let witchPowerCurrent = enemies.witch.power
 
 
 
 
-export const battle = async (battleWins, myCharAttack, updateWitchHp,updateWitchHpMax, updateWitchLevel,updateWitchAttack,updateWitcPower) => {
 
+export const battle = async (
+  battleWins,
+  updateWitchHp,
+  updateWitchHpMax,
+  updateWitchLevel,
+  updateWitchAttack,
+  updateWitchPower,
+  //
+  updateMyCharHp,
+  updateMyCharHpMax,
+  updateMyCharLevel,
+  updateMyCharAttack,
+  updateMyCharPower,
 
-  const enemyUpgradeSkills =  () => {
+) => {
+  const myCharUpgradeSkills = () => {
+    //life upgrade >
+    myCharhHpMax *= myCharLevel;
+    myCharHpCurrent = myCharhHpMax;
+    // //attack upgrade >
+    myCharAttackCurrent *= myCharLevel;
+    console.log(myCharAttackCurrent);
+    //power upgrade>
+    myCharPowerCurrent *= myCharLevel;
+    //return
+    updateMyCharHp(myCharHpCurrent);
+    updateMyCharHpMax(myCharhHpMax);
+    updateMyCharAttack(myCharAttackCurrent);
+    updateMyCharPower(myCharPowerCurrent)
+  }
+  const uplevelMyChar = () => {
+    try {
+      if (battleWins) {
+        myCharLevel += 1
+        myCharUpgradeSkills()
+        console.log('Your level Char : ', myCharLevel)
+        console.log('attack char', myCharAttackCurrent)
+        updateMyCharLevel(myCharLevel)
+      }
+    } catch (e) {
+      console.log('error : ', e)
+    }
 
+  }
+
+  //enemy functions
+  //--------------//
+
+  const enemyUpgradeSkills = () => {
+    //life upgrade >
     enemyWitchHpMax = enemyWitchHpMax * enemyWitchLevel;
     witchHpCurrent = enemyWitchHpMax;
-
-    // console.log(enemyWitchLevel)
-    // console.log(enemyWitchHpMax)
-    console.log(witchHpCurrent)
+    //attack upgrade >
+    witchAttackCurrent = witchAttackCurrent * enemyWitchLevel;
+    //power upgrade >
+    witchPowerCurrent *= enemyWitchLevel;
+    //return
 
     updateWitchHp(witchHpCurrent)
     updateWitchHpMax(enemyWitchHpMax)
-
+    updateWitchAttack(witchAttackCurrent)
+    updateWitchPower(witchPowerCurrent)
   }
-  
-  const uplevelEnemy =  () => {
+
+  const uplevelEnemy = () => {
     if (battleWins === true) {
       enemyWitchLevel += 1
-       enemyUpgradeSkills()
-      console.log(witchHpCurrent)
-      console.log('Your level : ',enemyWitchLevel)
+      enemyUpgradeSkills()
+      console.log('Your level : ', enemyWitchLevel)
       updateWitchLevel(enemyWitchLevel)
     }
   }
@@ -45,24 +103,27 @@ export const battle = async (battleWins, myCharAttack, updateWitchHp,updateWitch
   const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
   while (witchHpCurrent > 0 && !battleWins) {
-    
-    witchHpCurrent -= myCharAttack
-    
-    if (witchHpCurrent <= 0 && !battleWins ) {
-        battleWins = true
-        console.log('Battle finished with witch HP:', witchHpCurrent);
+
+
+    witchHpCurrent -= myCharAttackCurrent
+
+    if (witchHpCurrent <= 0 && !battleWins) {
+      battleWins = true
+      console.log('Battle finished with witch HP:', witchHpCurrent);
     } else {
       console.log('Continuous battle... Enemy life:', witchHpCurrent);
-   
+
     }
-    
+
     updateWitchHp(witchHpCurrent);
     await delay(1000);
- 
 
-  console.log("Battle Wins? :" ,battleWins)
-  
-  await uplevelEnemy()
+
+    console.log("Battle Wins? :", battleWins)
+
+    uplevelEnemy()
+    uplevelMyChar()
+
   }
   return battleWins
 }
